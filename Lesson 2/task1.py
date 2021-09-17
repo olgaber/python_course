c9d778312e974d6391b9a930e24881770eafdf65
olgaber <olgaber@users.noreply.github.com>
2021-09-17 20:26:14 +0300

    Homework done. @elpiankova

---
+++ Lesson 2/task1.py
@@ -0,0 +1,32 @@
+#1
+s1 = '54612028'
+s2 = ' text string with spaces. '
+
+print(s1.isnumeric())
+print(s2.isnumeric())
+
+#2
+print(s2.count(" "))
+
+#3
+print(s2.count("."))
+
+#4
+s3 = "Homework"
+print(s3.center(100, '0'))
+
+#5
+print(s2.title())
+
+#6
+print(s2.endswith("ing"))
+
+#7
+print(s2.index("a"))
+
+#8
+print(s2.split(" "))
+
+#9
+print(s2.strip())
+
